a15aa79d3f818a6f5879eb64a3f7ab51141c7731
Jingjing Peng <pengdeanna@gmail.com>
2025-11-18 18:45:09 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,29 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/Diashroom/lab11-JP.git
+# Partner 1: Jingjing
+# Partner 2: Jingjing
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
 
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return a / b
+
+def logarithm(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError("Arguments for logarithm must be positive.")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
 
