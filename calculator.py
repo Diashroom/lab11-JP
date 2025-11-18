f127256028188142961ad979699f52895ea97c21
Jingjing Peng <pengdeanna@gmail.com>
2025-11-18 18:58:07 -0500

    completed calculator functions and unit tests

--- calculator.py
+++ calculator.py
@@ -5,33 +5,25 @@
 
 import math
 
-def square_root(a):
-    if a < 0:
-        raise ValueError("Cannot take square root of negative number.")
-    return math.sqrt(a)
-
-def hypotenuse(a, b):
-    return math.hypot(a, b)
 
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
-    if b == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return a / b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
 
-def logarithm(a, b):
+def log(a, b):
     if a <= 0 or b <= 0:
-        raise ValueError("Arguments for logarithm must be positive.")
+        raise ValueError("Logarithm arguments must be positive")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
-
