a6b2a33e6c241063cecfc3d0d7bf14015041c338
Jingjing Peng <pengdeanna@gmail.com>
2025-11-18 19:21:22 -0500

    PLS WORK

--- test_calculator.py
+++ test_calculator.py
@@ -2,11 +2,12 @@
 # Partner 1: Jingjing
 # Partner 2: Jingjing
 
-
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
+
+    ########## PARTNER 2 TESTS ##########
 
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
@@ -31,7 +32,36 @@ class TestCalculator(unittest.TestCase):
         with self.assertRaises(ValueError):
             logarithm(-2, 10)
 
+    ########## PARTNER 1 TESTS ##########
+
+    def test_multiply(self):
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-1, 4), -4)
+        self.assertEqual(mul(0, 100), 0)
+
+    def test_divide(self):
+        self.assertAlmostEqual(div(2, 10), 5)   # because div(a,b) = b/a
+        self.assertAlmostEqual(div(3, 9), 3)
+        self.assertAlmostEqual(div(4, 20), 5)
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(2, -10)
+
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17)
+
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-9)
+
+        self.assertEqual(square_root(25), 5)
+        self.assertEqual(square_root(0), 0)
+
 # Do not touch this
 if __name__ == "__main__":
     unittest.main()
+
 
